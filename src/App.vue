06d6fbfc8d448b204567587dde2ff85b3423a7ed
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2018-12-23 20:52:00 -0500

    adding routes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+    <Header>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header clearfix">
+    <nav>
+      <ul class="nav nav-pills pull-right">
+      <li><router-link to="/">Inicio</router-link></li>
+      <li><router-link to="/ruta2">Ruta 2</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'Header'
+}
+</script>
